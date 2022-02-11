2299cc2cad51ba70a7410722b04379a80a5e2ce3
Kameron <kameronburden14@gmail.com>
2022-02-10 19:51:23 -0500

    changed image size

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign= "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '500px'
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for(i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign= 'left'
+}
+let shade = document.querySelector('.footer')
+shade.style.color = 'red'
 
 // Select some elements...
